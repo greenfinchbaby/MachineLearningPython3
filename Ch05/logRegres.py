3635a1894d44c878400c864dcbc42d2ac51dd861
Dongbo <greenfinchbaby@gmail.com>
2018-12-14 21:52:58 +0800

    dataIndex=list(range(m))

--- Ch05/logRegres.py
+++ Ch05/logRegres.py
@@ -66,7 +66,7 @@ def stocGradAscent1(dataMatrix, classLabels, numIter=150):
     m,n = shape(dataMatrix)
     weights = ones(n)   #initialize to all ones
     for j in range(numIter):
-        dataIndex = range(m)
+        dataIndex = list(range(m))
         for i in range(m):
             alpha = 4/(1.0+j+i)+0.0001    #apha decreases with iteration, does not 
             randIndex = int(random.uniform(0,len(dataIndex)))#go to 0 because of the constant
