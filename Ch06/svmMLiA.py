e6e0f8e1d04d608947e15702b4c7b885220b2804
Dongbo <greenfinchbaby@gmail.com>
2018-12-16 22:20:49 +0800

    Chapter6 svmMLiA.smoSimple() work

--- Ch06/svmMLiA.py
+++ Ch06/svmMLiA.py
@@ -49,9 +49,9 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
                 else:
                     L = max(0, alphas[j] + alphas[i] - C)
                     H = min(C, alphas[j] + alphas[i])
-                if L==H: print ("L==H"); continue
+                if L==H: print("L==H"); continue
                 eta = 2.0 * dataMatrix[i,:]*dataMatrix[j,:].T - dataMatrix[i,:]*dataMatrix[i,:].T - dataMatrix[j,:]*dataMatrix[j,:].T
-                if eta >= 0: print ( "eta>=0"); continue
+                if eta >= 0: print("eta>=0"); continue
                 alphas[j] -= labelMat[j]*(Ei - Ej)/eta
                 alphas[j] = clipAlpha(alphas[j],H,L)
                 if (abs(alphas[j] - alphaJold) < 0.00001): print ("j not moving enough"); continue
@@ -63,10 +63,10 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
                 elif (0 < alphas[j]) and (C > alphas[j]): b = b2
                 else: b = (b1 + b2)/2.0
                 alphaPairsChanged += 1
-                print( "iter: %d i:%d, pairs changed %d") % (iter,i,alphaPairsChanged)
+                print("iter: %d i:%d, pairs changed %d" % (iter, i, alphaPairsChanged))
         if (alphaPairsChanged == 0): iter += 1
         else: iter = 0
-        print ("iteration number: %d") % iter
+        print("iteration number: %d" % iter)
     return b,alphas
 
 def kernelTrans(X, A, kTup): #calc the kernel or transform data to a higher dimensional space
