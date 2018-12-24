21dd7def67f0d93c1f3fb7b7b1e6a3a1c0acd301
Dongbo <greenfinchbaby@gmail.com>
2018-12-24 21:43:34 +0800

    Chapter 6 modified "print" to  "print()“

--- Ch06/svmMLiA.py
+++ Ch06/svmMLiA.py
@@ -187,14 +187,14 @@ def testRbf(k1=1.3):
     svInd=nonzero(alphas.A>0)[0]
     sVs=datMat[svInd] #get matrix of only support vectors
     labelSV = labelMat[svInd];
-    print ("there are %d Support Vectors") % shape(sVs)[0]
+    print ("there are %d Support Vectors" % shape(sVs)[0])
     m,n = shape(datMat)
     errorCount = 0
     for i in range(m):
         kernelEval = kernelTrans(sVs,datMat[i,:],('rbf', k1))
         predict=kernelEval.T * multiply(labelSV,alphas[svInd]) + b
         if sign(predict)!=sign(labelArr[i]): errorCount += 1
-    print ("the training error rate is: %f") % (float(errorCount)/m)
+    print ("the training error rate is: %f" % (float(errorCount)/m))
     dataArr,labelArr = loadDataSet('testSetRBF2.txt')
     errorCount = 0
     datMat=mat(dataArr); labelMat = mat(labelArr).transpose()
@@ -203,7 +203,7 @@ def testRbf(k1=1.3):
         kernelEval = kernelTrans(sVs,datMat[i,:],('rbf', k1))
         predict=kernelEval.T * multiply(labelSV,alphas[svInd]) + b
         if sign(predict)!=sign(labelArr[i]): errorCount += 1    
-    print ("the test error rate is: %f") % (float(errorCount)/m)
+    print ("the test error rate is: %f" % (float(errorCount)/m))
     
 def img2vector(filename):
     returnVect = zeros((1,1024))
