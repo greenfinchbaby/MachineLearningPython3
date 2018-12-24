f046dcc7fbecd84e7a1cfa9af8c9f4ad8debe548
dongbo <greenfinchbaby@gmail.com>
2018-12-24 15:42:38 +0800

    Chapter 6 modified "print" to  "print()“

--- Ch06/svmMLiA.py
+++ Ch06/svmMLiA.py
@@ -159,17 +159,17 @@ def smoP(dataMatIn, classLabels, C, toler, maxIter,kTup=('lin', 0)):    #full Pl
         if entireSet:   #go over all
             for i in range(oS.m):        
                 alphaPairsChanged += innerL(i,oS)
-                print( "fullSet, iter: %d i:%d, pairs changed %d") % (iter,i,alphaPairsChanged)
+                print( "fullSet, iter: %d i:%d, pairs changed %d" % (iter,i,alphaPairsChanged))
             iter += 1
         else:#go over non-bound (railed) alphas
             nonBoundIs = nonzero((oS.alphas.A > 0) * (oS.alphas.A < C))[0]
             for i in nonBoundIs:
                 alphaPairsChanged += innerL(i,oS)
-                print ("non-bound, iter: %d i:%d, pairs changed %d") % (iter,i,alphaPairsChanged)
+                print ("non-bound, iter: %d i:%d, pairs changed %d" % (iter,i,alphaPairsChanged))
             iter += 1
         if entireSet: entireSet = False #toggle entire set loop
         elif (alphaPairsChanged == 0): entireSet = True  
-        print ("iteration number: %d" )% iter
+        print ("iteration number: %d" % iter)
     return oS.b,oS.alphas
 
 def calcWs(alphas,dataArr,classLabels):
